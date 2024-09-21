96a7340909ca9cb9b60886831c0fba292dbb023d
Luong Van Linh <luongvanlinh12@gmail.com>
2024-09-21 19:40:58 +0700

    first commit

--- js/app.js
+++ js/app.js
@@ -1,62 +1,73 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
- * 
-*/
+ *
+ */
 
+let sectionNav = document.querySelectorAll("section");
+let NavTag = document.getElementById("navbar_header");
+let sectionNavLength = sectionNav.length;
+let sectionNavPositions = [];
+let prePosition = 0;
+let currentPosition = 0;
 
 /**
  * End Global Variables
- * Start Helper Functions
- * 
-*/
+ *
+ */
 
+function scrollToSection(sectionID) {
+  window.scrollTo(0, sectionID);
+}
 
+// build the nav bar
+sectionNav.forEach((element, index) => {
+  let sectionName = element.getAttribute("data-nav");
+  let toOffSection = element.offsetTop + 30;
+  let liTag = document.createElement("li");
+  liTag.setAttribute("class", "menu_link" + index);
+  liTag.innerHTML = `<a onClick="scrollToSection(${toOffSection})">${sectionName}</a>`;
+  NavTag.appendChild(liTag);
+});
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+document.addEventListener("scroll", () => {
+  currentPosition = this.scrollY;
+  // Section Positions
+  sectionNavPositions = [];
+  sectionNav.forEach((element) =>
+    sectionNavPositions.push(element.getBoundingClientRect().top + 50)
+  );
 
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+  // Adding and removing active sections
+  let addIndex = sectionNavPositions.findIndex((element) => element > 0);
+  for (let i = 0; i < sectionNavLength; i++) {
+    if (addIndex === i) {
+      document.querySelector(".menu_link" + addIndex).classList.add("active");
+      document
+        .querySelector(`#section${addIndex + 1}`)
+        .classList.add("current-active-class");
+    } else {
+      document.querySelector(".menu_link" + i).classList.remove("active");
+      document.querySelector(`#section${i + 1}`).removeAttribute("class");
+    }
+  }
+});
